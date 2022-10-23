b026e82466f3a77f8970d99059d0c526d1bfd431
Anurag Wagh <56196363+Anuragwagh@users.noreply.github.com>
2022-10-23 12:34:10 +0530

    Add files via upload

---
+++ CPP/Area Of Isosceles Rectangle.cpp
@@ -0,0 +1,27 @@
+/*Area Of Isosceles Rectangle*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int b,h,a;
+    cout<<"Enter Base of Isosceles Triangle :";
+    cin>>b;
+    cout<<"Enter Height of Isosceles Triangle :";
+    cin>>h;
+    a=0.5*b*h;
+    cout<<endl;
+    cout<<"Area of Isosceles triangle is :"<<a;
+    return 0;
+}
+
+/*OUTPUT
+
+Enter Base of Isosceles Triangle :3
+Enter Height of Isosceles Triangle :2
+
+Area of Isosceles triangle is :3
+
+*/
